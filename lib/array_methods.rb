e94b7d30fec856dd4c50464fac0ae67ed283966d
haider <haider.shah@hotmail.com>
2019-08-29 03:23:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,24 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
- 
-  
+  counter=0
+  x=0 
+  y=array[counter]
+ while counter<array.length do
+   x=array[counter]
+ if x>=y 
+   y=x
+ end
+ counter=counter+1
+ end
+  if array.length==0
+    return nil
+  else
+ return y
+  end
 end
+
+
 
 def find_min_value(array)
 
