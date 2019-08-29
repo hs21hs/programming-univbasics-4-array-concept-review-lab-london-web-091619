f91985ffe8142c7cb611740786b1e280bd7534da
haider <haider.shah@hotmail.com>
2019-08-29 03:24:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -23,5 +23,20 @@ end
 
 
 def find_min_value(array)
-
+  counter=0
+  x=0 
+  y=array[counter]
+ while counter<array.length do
+   x=array[counter]
+ if x<=y 
+   y=x
+ end
+ counter=counter+1
+ end
+  if array.length==0
+    return nil
+  else
+ return y
+  end
 end
+
