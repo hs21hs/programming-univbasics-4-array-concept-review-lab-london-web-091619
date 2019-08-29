8b144d782ea7007ac363449244d76afc7ed4e24d
haider <haider.shah@hotmail.com>
2019-08-29 03:20:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,12 +3,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort.last
-new_max=0 
-array.each do |i|
-  if i> new_max
-    new_max=1 
-  end
+ 
+  
 end
 
 def find_min_value(array)
